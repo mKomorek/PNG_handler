4435faf16bb0574f3768eb6c5d9d0a8adf9b6ed4
Angstelk <angtbutan@gmail.com>
2020-04-07 00:09:45 +0200

    chyba si

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
 import os
+import sys 
 from png_handler import PNG_handler
 
-if __name__ == "__main__":
-    file_path = input("Enter the path of your file: ")
-    png_handler = PNG_handler(file_path)
+def submenu(png_handler):
+
+    choice = input("""
+                            F: Display file information
+                            D: Display chunk types 
+                            C: Display chunk informations 
+                            K: Display file
+                            T: Perform DFT and display output  
+                            N: Back to menu
+
+Your choice: """)
+
+     
+    if choice == "F" or choice =="f": 
+        png_handler.display_file_info()
+    elif choice == "D" or choice =="d":
+        png_handler.display_chunks_type()
+    elif choice=="C" or choice=="c":
+        png_handler.display_chunks_info()
     
-    os.system('clear')
-    png_handler.display_file_info()
-    png_handler.display_chunks_type()
-    png_handler.display_chunks_info()
-    png_handler.display_file()
-    png_handler.display_fourier_transform()
-    png_handler.display_from_reconstructed_idats()
-    png_handler.create_clean_file()
+    elif choice=="K" or choice=="k":
+        png_handler.display_file()
+    
+    elif choice=="T" or choice=="t":
+        png_handler.display_fourier_transform()
+    elif choice=="N" or choice=="n":
+        return 1 
+    else:
+        print("You must only select either F,D,C,K,T,N.")
+        print("Please try again")
+    submenu(png_handler)
+def menu():
+    print("************MAIN MENU**************")
+    print()
+
+    choice = input("""
+                      P: Path to input file    
+                      Q: Quit
+
+Your choice: """)
+
+    if choice == "P" or choice =="p":
+        file_path = input("Path: ")
+        png_handler = PNG_handler(file_path)
+        submenu(png_handler)
+    elif choice == "Q" or choice =="q":
+        return 1
+    else:
+        print("You must only select either P,Q.")
+        print("Please try again")
+    menu()
+
+if __name__ == "__main__":
+    menu()
+
